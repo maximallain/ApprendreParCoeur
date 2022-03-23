ea1502d91bc946874a262768385dacf3c8b25eee
maxi_malain <maxime.allain.8@gmail.com>
2022-03-23 19:52:28 +0100

    feat: create and inject GuessWordContainer

---
+++ src/components/GuessWordContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './GuessWordContainer.css';
+
+const GuessWordContainer = () => {
+  return (
+    <div className='container'>
+        GuessWordContainer
+    </div>
+    )
+}
+  
+export default GuessWordContainer;
