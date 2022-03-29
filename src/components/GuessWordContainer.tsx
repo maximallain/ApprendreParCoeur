7c8afa3e6760199d93063fd22e1a3f249bee2e12
maxi_malain <maxime.allain.8@gmail.com>
2022-03-29 11:35:35 +0200

    feat: add buttons into GuessWordContainer

--- src/components/GuessWordContainer.tsx
+++ src/components/GuessWordContainer.tsx
@@ -4,7 +4,18 @@ import './GuessWordContainer.css';
 const GuessWordContainer = () => {
   return (
     <div className='container'>
-        GuessWordContainer
+        <div className="header">
+          (Consigne...) Quelle est la définition du mot ?
+        </div>
+
+        <div className='body'>
+            Aristocrate
+        </div>
+
+        <div className='footer'>
+          <button className='postive-button'>Je sais !</button>
+          <button className='negative-button'>Je sais pas</button>
+        </div>
     </div>
     )
 }
