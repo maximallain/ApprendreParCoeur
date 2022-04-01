3ea670a64cf0166523c5fa40dff9a463a24671df
maxi_malain <maxime.allain.8@gmail.com>
2022-04-01 17:11:26 +0200

    design: arrange GuessWordContainer header and buttons

--- src/components/GuessWordContainer.tsx
+++ src/components/GuessWordContainer.tsx
@@ -2,15 +2,13 @@ import "./GuessWordContainer.css";
 
 const GuessWordContainer = () => {
   return (
-    <div className="card">
-      <div className="card-header">
-        <div className="card-header-title">Définition</div>
-      </div>
-
-      <div className="card-content">
-        <div className="title is-3 ">Erudit</div>
-        <button className="button is-primary">Je sais !</button>
-        <button className="button is-warning">Je sais pas</button>
+    <div className="card guess-word-container">
+      <div className="card-content is-flex is-flex-direction-column is-justify-content-space-between">
+        <div className="title is-flex-1 is-1 has-text-centered has-text-weight-bold">Erudit</div>
+        <div className="buttons-container is-flex is-justify-content-center">
+          <button className="button is-primary is-size-4">Je sais !</button>
+          <button className="button is-warning is-size-4">Je sais pas</button>
+        </div>
       </div>
     </div>
   );
