99a9f3e3d5aec539eaf6176276341c6b73cb0b38
maxi_malain <maxime.allain.8@gmail.com>
2022-04-01 17:35:08 +0200

    feat: when clicking on "I know" it proposes two new buttons

--- src/components/GuessWordContainer.tsx
+++ src/components/GuessWordContainer.tsx
@@ -1,14 +1,79 @@
+import { useMemo, useState } from "react";
 import "./GuessWordContainer.css";
 
 const GuessWordContainer = () => {
+  const [hasAnswered, setHasAnswered] = useState(false);
+  const [knowsTheDefinition, setKnowsTheDefinition] = useState<boolean | null>(null);
+
+  const answer = () => setHasAnswered(true);
+  const answerAndKnow = () => {
+    answer();
+    setKnowsTheDefinition(true);
+  };
+
+  const answerAndDontKnow = () => {
+    answer();
+    setKnowsTheDefinition(false);
+  };
+
+  const definitionContainer = (
+    <p className="has-text-centered">
+      Qui a des connaissances approfondies dans une matière, en particulier des connaissances historiques.
+    </p>
+  );
+
+  const answerButtons = (
+    <div className="columns">
+      <div className="column is-centered">
+        <button className="button is-primary is-large has-text-weight-bold is-fullwidth" onClick={answerAndKnow}>
+          Je sais !
+        </button>
+      </div>
+
+      <div className="column">
+        <button className="button is-warning is-large has-text-weight-bold is-fullwidth" onClick={answerAndDontKnow}>
+          Je sais pas
+        </button>
+      </div>
+    </div>
+  );
+
+  const answerAndKnowsButtons = (
+    <div className="columns">
+      <div className="column is-centered">
+        <button
+          className="button is-primary is-large has-text-weight-bold is-fullwidth"
+          onClick={() => console.log("Je confirme que je sais")}
+        >
+          Je confirme que je sais
+        </button>
+      </div>
+
+      <div className="column">
+        <button
+          className="button is-warning is-large has-text-weight-bold is-fullwidth"
+          onClick={() => console.log("Ah ! En fait, non, je ne l'avais pas")}
+        >
+          Ah ! En fait, non, je ne l{"'"}avais pas
+        </button>
+      </div>
+    </div>
+  );
+
+  const buttons = useMemo(() => {
+    if (knowsTheDefinition === true) {
+      return answerAndKnowsButtons;
+    }
+    return answerButtons;
+  }, [knowsTheDefinition]);
+
   return (
     <div className="card guess-word-container">
       <div className="card-content is-flex is-flex-direction-column is-justify-content-space-between">
-        <div className="title is-flex-1 is-1 has-text-centered has-text-weight-bold">Erudit</div>
-        <div className="buttons-container is-flex is-justify-content-center">
-          <button className="button is-primary is-size-4">Je sais !</button>
-          <button className="button is-warning is-size-4">Je sais pas</button>
-        </div>
+        <div className="title is-1 has-text-centered has-text-weight-bold mt-6 mb-0">Erudit</div>
+
+        {hasAnswered && definitionContainer}
+        {buttons}
       </div>
     </div>
   );
