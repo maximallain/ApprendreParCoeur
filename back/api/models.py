9dfc550efc09921b9d29e4a2e5e01592c8a88d7e
maxi_malain <maxime.allain.8@gmail.com>
2022-11-10 12:31:48 +0100

    create word_definition model

--- back/api/models.py
+++ back/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class WordDefinition(models.Model):
+    word = models.CharField(max_length=80)
+    definition = models.TextField()
+    updated = models.DateTimeField(auto_now = True, blank = True)
+
+
+    def __str__(self):
+        return self.word
