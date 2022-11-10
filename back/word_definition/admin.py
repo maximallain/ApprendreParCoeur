475a133982015d3241c018062b0c8cffb30c4be5
maxi_malain <maxime.allain.8@gmail.com>
2022-11-10 12:12:48 +0100

    remove back project

--- back/word_definition/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import WordDefinition
-
-admin.site.register(WordDefinition)
--- back/word_definition/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class WordDefinition(models.Model):
-  word = models.CharField(max_length=80)
-  definition = models.TextField()
