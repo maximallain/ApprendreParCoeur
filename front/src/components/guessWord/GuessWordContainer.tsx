e804abe7a98260a54371d71377b69da8bf85222d
maxi_malain <maxime.allain.8@gmail.com>
2022-11-22 09:24:28 +0100

    front: use query and call back to get wordDefinitions list

--- front/src/components/guessWord/GuessWordContainer.tsx
+++ front/src/components/guessWord/GuessWordContainer.tsx
@@ -1,5 +1,4 @@
 import { useCallback, useMemo, useState } from "react";
-import { useQuery } from "react-query";
 import "./GuessWordContainer.css";
 import WordCardContent from "./WordCardContent";
 
@@ -8,66 +7,25 @@ export type WordDefinition = {
   definition: string;
 };
 
-const WORDS_DEFINITIONS: WordDefinition[] = [
-  { word: "cimaise", definition: "Moulure qui forme la partie supérieure d'une corniche." },
-  { word: "acuité", definition: "Caractère aigu, intense. Degré de sensibilité (d'un sens)." },
-  { word: "prosaïque", definition: "Qui tient trop de la prose. Qui manque d'élégance." },
-  { word: "éphèbe", definition: "Adolescent grec. Jeune homme." },
-  {
-    word: "phéromone",
-    definition:
-      "Molécule chimique produite par un organisme, qui induit un comportement spécifique chez un autre membre de la même espèce.",
-  },
-  {
-    word: "colluvion",
-    definition: "Dépôt de sédiments qui se forme sur une petite distance et en fine couche sur un versant.",
-  },
-  {
-    word: "syntagme",
-    definition:
-      "En linguistique, le syntagme est un constituant syntaxique et sémantique de la phrase. On l'appelle aussi groupe ou entité car il est composé d'un ou plusieurs mots allant jusqu'à la phrase simple.",
-  },
-  {
-    word: "parataxe",
-    definition:
-      "Juxtaposition de propositions sans mot de liaison. Exemple : J’ai pris l’autobus à 2 heures. Il faisait très chaud. J’ai mangé au restaurant, chez Céleste, comme d’habitude. (Albert Camus, L'Étranger)",
-  },
-  {
-    word: "asyndète",
-    definition: "Absence de liaison entre deux termes ou groupes de termes en rapport étroit (ex. bon pied bon œil).",
-  },
-  {
-    word: "anaphore",
-    definition:
-      "Répétition d'un mot en tête de plusieurs membres de phrase, pour obtenir un effet de renforcement ou de symétrie. Exemple : Paris ! Paris outragé ! Paris brisé ! Paris martyrisé !",
-  },
-];
+type Props = {
+  wordDefinitions: WordDefinition[];
+};
 
-const GuessWordContainer = () => {
+const GuessWordContainer = ({ wordDefinitions }: Props) => {
   const [wordDefinitionIndex, setWordDefinitionIndex] = useState(0);
   const [isListTerminated, setIsListTerminated] = useState(false);
   const [score, setScore] = useState(0);
 
-  const currentWordDefinition = useMemo(() => WORDS_DEFINITIONS[wordDefinitionIndex], [wordDefinitionIndex]);
-  const progress = useMemo(() => 100 * (wordDefinitionIndex / WORDS_DEFINITIONS.length), [wordDefinitionIndex]);
+  const currentWordDefinition = useMemo(() => wordDefinitions[wordDefinitionIndex], [wordDefinitionIndex]);
+  const progress = useMemo(() => 100 * (wordDefinitionIndex / wordDefinitions.length), [wordDefinitionIndex]);
 
   const increment = useCallback((wordDefinitionIndex: number) => {
-    if (wordDefinitionIndex + 1 >= WORDS_DEFINITIONS.length) {
+    if (wordDefinitionIndex + 1 >= wordDefinitions.length) {
       setIsListTerminated(true);
     } else {
       setWordDefinitionIndex(wordDefinitionIndex + 1);
     }
   }, []);
-
-  const { isLoading, error, data } = useQuery("repoData", () =>
-    fetch("/api/word_definitions/?format=json").then((res) => {
-      console.log("res", res);
-      return res.json();
-    })
-  );
-  console.log("isLoading", isLoading);
-  console.log("data", data);
-  console.log("error", error);
 
   const displayNextWord = (incrementingScore: boolean) => {
     increment(wordDefinitionIndex);
@@ -78,7 +36,7 @@ const GuessWordContainer = () => {
     <div className="card guess-word-container">
       {isListTerminated ? (
         <div className="title is-1 has-text-centered has-text-weight-bold mt-6 mb-0">
-          Ton score : {score}/{WORDS_DEFINITIONS.length}
+          Ton score : {score}/{wordDefinitions.length}
         </div>
       ) : (
         <WordCardContent wordDefinition={currentWordDefinition} progress={progress} displayNextWord={displayNextWord} />
--- front/src/components/layout/Content.tsx
+++ front/src/components/layout/Content.tsx
@@ -1,18 +1,34 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 import GuessWordContainer from "../guessWord/GuessWordContainer";
 
 const Content = () => {
   const [hasBegun, setHasBegun] = useState(false);
 
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch("http://localhost:8000/api/word_definitions/?format=json").then((res) => res.json())
+  );
+
+  // Handle error
+  if (isLoading) {
+    return (
+      <div>
+        <p>Chargement...</p>
+      </div>
+    );
+  }
+
+  const beginButton = (
+    <button className="button is-info is-size-4" onClick={() => setHasBegun(true)}>
+      Commencer
+    </button>
+  );
+
+  const content = isLoading ? <div>Chargement des mots...</div> : <GuessWordContainer wordDefinitions={data} />;
+
   return (
     <div className="container is-flex is-justify-content-center is-align-items-center">
-      {hasBegun ? (
-        <GuessWordContainer />
-      ) : (
-        <button className="button is-info is-size-4" onClick={() => setHasBegun(true)}>
-          Commencer
-        </button>
-      )}
+      {hasBegun ? content : beginButton}
     </div>
   );
 };
